c1232613bac1478aee27e3934269b92401ac1cff
haragg2 <hardikagg210@gmail.com>
2017-08-29 16:28:31 +0530

    Added queue and combined transmit receiving block

--- Arduino/tx_rtos/tx_rtos.ino
+++ Arduino/tx_rtos/tx_rtos.ino
@@ -1,4 +1,5 @@
 #include <Arduino_FreeRTOS.h>
+#include <queue.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -7,15 +8,20 @@ RF24 radio(7, 4);
 
 const byte Addr[6] = "00001";
 
-// define two tasks for Transmitter and Receiver
-void Receive( void *pvParameters );
-void Transmit( void *pvParameters );
+// define two tasks for transmission and reading values
+void Joystick_read( void *pvParameters );
+void Transmission( void *pvParameters );
 
-UBaseType_t priority=0;
-TaskHandle_t tx;
+QueueHandle_t dataQueue;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
   
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -26,19 +32,19 @@ void setup() {
   radio.begin();
   radio.setRetries(15, 15);
 
+  dataQueue = xQueueCreate( 10, 4 );
 
   xTaskCreate(
-    Transmit
-    ,  (const portCHAR *) "Transmitter"
+    Transmission
+    ,  (const portCHAR *) "Transmit and Receive"
     ,  256  // Stack size
     ,  NULL
-    ,  1  // Priority
-    ,  &tx );
-  
-  // Now set up two tasks to run independently.
-  xTaskCreate(
-    Receive
-    ,  (const portCHAR *)"Receiver"   // A name just for humans
+    ,  3  // Priority
+    ,  NULL );
+
+    xTaskCreate(
+    Joystick_read
+    ,  (const portCHAR *)"Read Joystick" 
     ,  256  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
     ,  2 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
@@ -57,12 +63,14 @@ void loop()
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void Receive(void *pvParameters)  // This is a task.
+
+
+void Transmission(void *pvParameters)  // This is a data transmitting and receiving task.
 {
+  byte data_tx[4]={0,0,0,0};
   byte data_rx=0;
   for (;;) // A Task shall never return or exit.
   {
-    //Serial.print("R");
     // Open a pipe for reading
     radio.openReadingPipe(0, Addr);
     radio.startListening();
@@ -72,33 +80,37 @@ void Receive(void *pvParameters)  // This is a task.
        Serial.print("RX from rx:  ");
        Serial.println(data_rx);
      }
-     delay(15);
-     vTaskPrioritySet( tx, 3 );
-     taskYIELD();
+    delay(15);
+    
+    radio.openWritingPipe(Addr);
+    radio.stopListening();
+    if(xQueueReceive( dataQueue, data_tx, portMAX_DELAY )){
+      radio.write(data_tx, 4);
+      Serial.print((data_tx[0]*255)+data_tx[1]);
+      Serial.print("  :  ");
+      Serial.println((data_tx[2]*255)+data_tx[3]);
+    }
+    else{
+      taskYIELD();
+     }
+    delay(15);
     }
 }
 
-
-void Transmit(void *pvParameters)  // This is a task.
+void Joystick_read(void *pvParameters)  // This is a joystick value reading task.
 {
-  byte data_tx=0;
+  byte data[4] = {0,0,0,0};
+  int Left_ctr =0;
+  int Right_ctr =0;
+  
   for (;;)
   { 
-    //Serial.println("T");
-   
-    // Open a pipe for writing
-    radio.openWritingPipe(Addr);
-    radio.stopListening();
-
-    data_tx=(data_tx+1)%255;
-    Serial.print("TX to rx:  ");
-    Serial.println(data_tx);
-    
-    radio.write(&data_tx, sizeof(byte));
-    delay(15);
-    vTaskPrioritySet( tx, 1 );
-    taskYIELD();
-    
+    Left_ctr = analogRead(A0);
+    Right_ctr = analogRead(A1);
+    data[0] =Left_ctr/255;
+    data[1] =Left_ctr-(data[0]*255);
+    data[2] =Right_ctr/255;
+    data[3] =Right_ctr-(data[2]*255);
+    xQueueSend(dataQueue,data,portMAX_DELAY);
   }
 }
-
